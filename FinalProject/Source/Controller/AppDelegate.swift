9d8769963bf87a05c9df7a3759d0280a70eb385f
Charles Augustine <charles@concentricsky.com>
2015-11-04 14:32:24 -0800

    Final Project template

---
+++ FinalProject/Source/Controller/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FinalProject
+//
+//  Created by Charles Augustine.
+//
+//
+
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		return true
+	}
+
+	// MARK: Properties
+	var window: UIWindow?
+}
+
