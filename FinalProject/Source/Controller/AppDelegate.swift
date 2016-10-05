6c815ba2376e624c64a2ab716fee44e716fc29bb
Nate Nichols <natsamnic@gmail.com>
2016-10-05 12:36:40 -0700

    CAN TALK TO API ;)

--- FinalProject/Source/Controller/AppDelegate.swift
+++ FinalProject/Source/Controller/AppDelegate.swift
@@ -9,7 +9,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(application: UIApplication,
 	                 didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        let string = "https://lumaa.co/api/feedingprogram/"
+        let url = NSURL(string: string)
+        let request = NSMutableURLRequest(URL: url!)
+        request.setValue("Token <YOURTOKENHERE>", forHTTPHeaderField: "Authorization")
+        request.HTTPMethod = "GET"
+        let session = NSURLSession.sharedSession()
         
+        let tache = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            if let antwort = response as? NSHTTPURLResponse {
+                let code = antwort.statusCode
+                print(code)
+            }
+        }
+        tache.resume()
         
         
 		return true
